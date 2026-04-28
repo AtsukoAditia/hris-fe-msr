f412b9a4c2111cad6769cacf46b5c36b7d0de28d
Aditia Nugraha <atsuko.aditia@gmail.com>
2026-04-28 16:29:02 +0700

    feat: update MainLayout - mobile sidebar toggle + children prop

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,21 +1,23 @@
-import { Outlet } from 'react-router-dom'
+import { useState } from 'react'
 import Sidebar from './Sidebar'
 import Navbar from './Navbar'
 
-const MainLayout = () => {
+const MainLayout = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
   return (
     <div className="flex h-screen bg-gray-50 overflow-hidden">
       {/* Sidebar */}
-      <Sidebar />
+      <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
       {/* Main Content */}
       <div className="flex-1 flex flex-col overflow-hidden">
         {/* Top Navbar */}
-        <Navbar />
+        <Navbar onMenuToggle={() => setSidebarOpen(!sidebarOpen)} />
 
         {/* Page Content */}
         <main className="flex-1 overflow-y-auto p-6">
-          <Outlet />
+          {children}
         </main>
       </div>
     </div>
