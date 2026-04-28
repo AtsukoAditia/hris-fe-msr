d288de36867a84668efcb26494f4e68b23f11dee
Aditia Nugraha <atsuko.aditia@gmail.com>
2026-04-28 15:44:54 +0700

    feat: add src/store/attendanceStore.js

---
+++ src/store/attendanceStore.js
@@ -0,0 +1,16 @@
+import { create } from 'zustand'
+
+const useAttendanceStore = create((set) => ({
+  todayAttendance: null,
+  attendanceList: [],
+  isLoading: false,
+  checkinStatus: null, // 'checkin' | 'checkout' | null
+
+  setTodayAttendance: (data) => set({ todayAttendance: data }),
+  setAttendanceList: (list) => set({ attendanceList: list }),
+  setCheckinStatus: (status) => set({ checkinStatus: status }),
+  setLoading: (isLoading) => set({ isLoading }),
+  reset: () => set({ todayAttendance: null, checkinStatus: null }),
+}))
+
+export default useAttendanceStore
