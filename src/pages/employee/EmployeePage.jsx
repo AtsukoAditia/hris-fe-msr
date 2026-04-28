4730d232766b6711428898b29f47dfda5b865175
Aditia Nugraha <atsuko.aditia@gmail.com>
2026-04-28 15:52:43 +0700

    feat: add src/pages/employee/EmployeePage.jsx

---
+++ src/pages/employee/EmployeePage.jsx
@@ -0,0 +1,14 @@
+// TODO: Implement employee management page
+const EmployeePage = () => {
+  return (
+    <div className="space-y-6">
+      <h1 className="text-2xl font-bold text-gray-900">Data Karyawan</h1>
+      <div className="bg-white rounded-xl shadow-sm p-6 border border-gray-100">
+        <p className="text-gray-500">Halaman manajemen karyawan akan diimplementasikan di sini.</p>
+        {/* TODO: Employee list, add/edit/delete employee, employee profile */}
+      </div>
+    </div>
+  )
+}
+
+export default EmployeePage
