c71cad2a2805b2fc9f3961bd4f2096706191885a
Aditia Nugraha <atsuko.aditia@gmail.com>
2026-06-09 16:04:15 +0700

    feat: add employee face enrollment service

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -6,6 +6,16 @@ const employeeService = {
   create: (data) => api.post('/employees', data),
   update: (id, data) => api.put(`/employees/${id}`, data),
   delete: (id) => api.delete(`/employees/${id}`),
+  enrollFace: (id, file) => {
+    const formData = new FormData()
+    formData.append('face_image', file)
+
+    return api.post(`/employees/${id}/face-enrollment`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+  },
 }
 
 export default employeeService
