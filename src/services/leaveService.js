9fd73b1e7447026c51c7ae06ae27193d3fbe390d
Aditia Nugraha <atsuko.aditia@gmail.com>
2026-06-10 18:40:41 +0700

    feat: stabilize leave service api

--- src/services/leaveService.js
+++ src/services/leaveService.js
@@ -1,26 +1,14 @@
-import api from '../lib/axios';
+import api from '../lib/axios'
 
 const leaveService = {
-  // Get my own leave requests (employee)
   getMyLeaves: (params = {}) => api.get('/leaves/my', { params }),
-
-  // Get all leave requests (admin/hr/manager)
   getAll: (params = {}) => api.get('/leaves', { params }),
-
-  // Get leave by ID
   getById: (id) => api.get(`/leaves/${id}`),
-
-  // Create leave request
   create: (data) => api.post('/leaves', data),
+  cancel: (id) => api.delete(`/leaves/${id}`),
+  approve: (id, data = {}) => api.post(`/leaves/${id}/approve`, data),
+  reject: (id, data = {}) => api.post(`/leaves/${id}/reject`, data),
+  getBalance: (params = {}) => api.get('/leaves/balance', { params }),
+}
 
-  // Approve leave request (manager/admin) - POST
-  approve: (id, data) => api.post(`/leaves/${id}/approve`, data),
-
-  // Reject leave request (manager/admin) - POST
-  reject: (id, data) => api.post(`/leaves/${id}/reject`, data),
-
-  // Get leave balance (employee's own balance)
-  getBalance: () => api.get('/leaves/balance'),
-};
-
-export default leaveService;
+export default leaveService
