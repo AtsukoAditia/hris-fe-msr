54d26e8a9389f109845715c8eb5235b0be856710
Aditia Nugraha <atsuko.aditia@gmail.com>
2026-06-20 15:01:30 +0700

    fix: normalize branch list query params

--- src/services/branchService.js
+++ src/services/branchService.js
@@ -1,7 +1,8 @@
 import api from '../lib/axios'
+import { normalizeQueryParams } from './queryParams'
 
 const branchService = {
-  getAll: (params = {}) => api.get('/branches', { params }),
+  getAll: (params = {}) => api.get('/branches', { params: normalizeQueryParams(params) }),
   getById: (id) => api.get(`/branches/${id}`),
   create: (data) => api.post('/branches', data),
   update: (id, data) => api.put(`/branches/${id}`, data),
