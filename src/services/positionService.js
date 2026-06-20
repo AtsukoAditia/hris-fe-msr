bf446794eb985a3a5ac2f94af0ba39d741438bf1
Aditia Nugraha <atsuko.aditia@gmail.com>
2026-06-20 15:01:14 +0700

    fix: normalize position list query params

--- src/services/positionService.js
+++ src/services/positionService.js
@@ -1,7 +1,8 @@
 import api from '../lib/axios'
+import { normalizeQueryParams } from './queryParams'
 
 const positionService = {
-  getAll: (params = {}) => api.get('/positions', { params }),
+  getAll: (params = {}) => api.get('/positions', { params: normalizeQueryParams(params) }),
   getById: (id) => api.get(`/positions/${id}`),
   create: (data) => api.post('/positions', data),
   update: (id, data) => api.put(`/positions/${id}`, data),
