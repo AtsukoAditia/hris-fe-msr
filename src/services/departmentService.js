d24bb357df8ccc810288f91a72df128ecead760a
Aditia Nugraha <atsuko.aditia@gmail.com>
2026-06-20 15:00:50 +0700

    fix(master-data): serialize department boolean filters

--- src/services/departmentService.js
+++ src/services/departmentService.js
@@ -1,7 +1,8 @@
 import api from '../lib/axios'
+import { normalizeQueryParams } from './queryParams'
 
 const departmentService = {
-  getAll: (params = {}) => api.get('/departments', { params }),
+  getAll: (params = {}) => api.get('/departments', { params: normalizeQueryParams(params) }),
   getById: (id) => api.get(`/departments/${id}`),
   create: (data) => api.post('/departments', data),
   update: (id, data) => api.put(`/departments/${id}`, data),
